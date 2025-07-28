a7cc9633342aa40fe7ffd425e3cbe7b50a215777
Eclypxxe <elvientoenlasvelas@gmail.com>
2025-07-28 14:04:42 +0200

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import GenogramaEmocionalApp from './GenogramaEmocionalApp';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <GenogramaEmocionalApp />
+  </React.StrictMode>
+);
