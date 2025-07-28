9d5a43e84425179c15f0c514315e4a58c952b827
Eclypxxe <elvientoenlasvelas@gmail.com>
2025-07-28 20:54:52 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,20 @@
 self.addEventListener('install', (e) => {
+  console.log('🛠 Instalando service worker...');
   e.waitUntil(
-    caches.open('genomotion-cache').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/icon-192.png',
-        '/icon-512.png'
-      ]);
-    })
+    caches.open('genomotion-v1').then((cache) =>
+      cache.addAll([
+        '/genomotion/',
+        '/genomotion/index.html',
+        '/genomotion/manifest.json',
+        '/genomotion/icon-192.png',
+        '/genomotion/icon-512.png',
+      ])
+    )
   );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+    caches.match(e.request).then((response) => response || fetch(e.request))
   );
 });
